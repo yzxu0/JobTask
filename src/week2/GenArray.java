b02ce9622667c8c1bb0f1e507eafcfec904b0336
yzxu <yzxu@beyondh.com>
2017-10-11 21:40:31 +0800

    添加关键代码注释

--- src/week2/GenArray.java
+++ src/week2/GenArray.java
@@ -4,7 +4,7 @@ public class GenArray {
     public int[] GenArrayProcess() {
         int[] arr = new int[1000];
         for (int i = 0; i < 1000; i++) {
-            arr[i] = i * (i % 2 == 1 ? 1 : -1);
+            arr[i] = i * (i % 2 == 1 ? 1 : -1); // 对 i 取2的余数;当余数为1,乘以1;否则乘以-1
         }
         return arr;
     }
